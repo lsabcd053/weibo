21afce30be21ef7f92a258bee117e8d045908ed6
lsabcd053 <lidingbo_1989@163.com>
2014-02-04 22:16:47 +0800

    second commit

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+define( "WB_AKEY" , '3880719658' );
+define( "WB_SKEY" , '539421c8b2bec2ecf86c2a811b0d9ffd' );
+define( "CANVAS_PAGE" , "http://apps.weibo.com/lsabcdiie" );
